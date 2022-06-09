315bf91da24dc1a352a2244dfda90f076b297b01
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-09 18:32:04 -0500

    feature: update healthcheck endpoint

--- internal/api/controllers/healthcheck.go
+++ internal/api/controllers/healthcheck.go
@@ -3,10 +3,19 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/api/schemas"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
-// HealthcheckGETHandler handles "/api/healthcheck" endpoint. TODO: for now, just return plain text.
-func HealthcheckGETHandler(c *gin.Context) {
-	c.String(http.StatusOK, "status: available\nfull path:%s", c.FullPath())
+// HealthcheckHandler handles "GET /api/healthcheck" endpoint. TODO: for now, just return plain text.
+func HealthcheckHandler(c *gin.Context) {
+	hc := schemas.Healthcheck{
+		Status: "available",
+		SystemInfo: schemas.SystemInfo{
+			Env:     viper.GetString("env"),
+			Version: viper.GetString("version"),
+		},
+	}
+	c.JSON(http.StatusOK, hc)
 }
---
+++ internal/api/schemas/healthcheck.go
@@ -0,0 +1,11 @@
+package schemas
+
+type SystemInfo struct {
+	Env     string `json:"env"`
+	Version string `json:"version"`
+}
+
+type Healthcheck struct {
+	Status     string     `json:"status"`
+	SystemInfo SystemInfo `json:"system_info"`
+}
