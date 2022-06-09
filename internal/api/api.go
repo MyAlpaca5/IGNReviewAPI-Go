3e7dd0cd37c0ec15ccfd706154c6dbfc234835f3
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-09 18:31:29 -0500

    refactor: clean up api main entrance

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,33 +1,14 @@
 package api
 
 import (
-	"flag"
-
+	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/api/config"
 	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/api/router"
+	"github.com/spf13/viper"
 )
 
-const Version = "1.0.0"
-
-// Config stores all configuration related data.
-type config struct {
-	port string
-	env  string
-}
-
-func NewConfig() config {
-	var cfg config
-
-	// Read value from command line parameters and set them into config struct
-	flag.StringVar(&cfg.port, "port", "50001", "API server port number")
-	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|staging|prod)")
-	flag.Parse()
-
-	return cfg
-}
-
 func Run() {
-	cfg := NewConfig()
+	config.Setup()
 	router := router.NewRouter()
 
-	router.Run(":" + cfg.port)
+	router.Run(":" + viper.GetString("port"))
 }
