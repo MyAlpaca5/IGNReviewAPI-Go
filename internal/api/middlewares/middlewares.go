688ac477168912fd9e3b163276311918002c94c8
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-10 16:39:44 -0500

    feat: add BodySizeLimiter middleware

---
+++ internal/api/middlewares/middlewares.go
@@ -0,0 +1,21 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+
+	r_errors "github.com/MyAlpaca5/IGNReviewAPI-Go/internal/api/errors"
+	"github.com/gin-gonic/gin"
+)
+
+// BodySizeLimiter will limit how large the request body can be in bytes
+func BodySizeLimiter(maxSize int64) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.ContentLength > maxSize {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, r_errors.ResponseError{StatusCode: http.StatusInternalServerError, Message: fmt.Sprintf("Error - request body exceed %d bytes", maxSize)})
+			return
+		}
+
+		c.Next()
+	}
+}
