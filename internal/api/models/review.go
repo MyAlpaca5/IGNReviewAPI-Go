860eceed9dc9edbbffa29d8b2f9396f38090bc3d
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-11 00:24:50 -0500

    refactor: restructure data model

---
+++ internal/api/models/review.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+)
+
+type Review struct {
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at" binding:"required"`
+	UpdatedAt   time.Time `json:"updated_at" binding:"required,gtfield=CreatedAt"`
+	ReviewURL   string    `json:"review_url" binding:"required,ignurl"`
+	ReviewScore float32   `json:"review_score" binding:"required,min=0,max=10"`
+	MediaType   string    `json:"media_type"`
+	GenreList   []string  `json:"genre_list,omitempty"`
+	CreatorList []string  `json:"creator_list,omitempty"`
+}
