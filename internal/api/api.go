82dcc5b2752a3d5950aac9556ffe52ac7ae27da8
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-11 14:45:52 -0500

    chore: init db connection

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,14 +1,29 @@
 package api
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/api/router"
-	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/api/utils"
+	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/db"
+	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/utils"
 	"github.com/spf13/viper"
 )
 
 func Run() {
+	// TODO: move this function into other file out of api folder
+	// initialize viper for configuration
 	utils.InitConfig()
-	router := router.NewRouter()
 
+	// initialize conn variable for database connection
+	_, close, err := db.NewPGXPool()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal - %v", err)
+		os.Exit(1)
+	}
+	defer close()
+
+	// initialize router
+	router := router.NewRouter()
 	router.Run(":" + viper.GetString("port"))
 }
