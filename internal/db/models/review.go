53a19485b7dedc63a757f37416144f0008609b75
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-11 13:22:41 -0500

    chore: update validator comdition
    created_at and updated_at can be equal

--- internal/db/models/review.go
+++ internal/db/models/review.go
@@ -8,7 +8,7 @@ type Review struct {
 	Name        string    `json:"name" binding:"required"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at" binding:"required"`
-	UpdatedAt   time.Time `json:"updated_at" binding:"required,gtfield=CreatedAt"`
+	UpdatedAt   time.Time `json:"updated_at" binding:"required,gtefield=CreatedAt"`
 	ReviewURL   string    `json:"review_url" binding:"required,ignurl"`
 	ReviewScore float32   `json:"review_score" binding:"required,min=0,max=10"`
 	MediaType   string    `json:"media_type"`
