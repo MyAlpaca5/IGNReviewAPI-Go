4110cbd13f2d91b922d9dded8d4a2848af369af3
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-15 15:59:03 -0500

    feat: add more migration

--- migrations/000001_create_reviews_table.up.sql
+++ migrations/000001_create_reviews_table.up.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS reviews (
     id serial PRIMARY KEY,
-    name text NOT NULL,
-    description text,
     created_at timestamp NOT NULL DEFAULT NOW(),
     updated_at timestamp NOT NULL DEFAULT NOW(),
+    name text NOT NULL,
+    description text,
     review_url text NOT NULL DEFAULT 'https://www.ign.com/articles',
     review_score real NOT NULL DEFAULT 0.0,
     media_type text,
-    genre_list text[],
-    creator_list text[],
-    version bigint NOT NULL DEFAULT 0
+    genre_list text[] DEFAULT '{}',
+    creator_list text[] DEFAULT '{}'
 );
---
+++ migrations/000004_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY,
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    updated_at timestamp NOT NULL DEFAULT NOW(),
+    username text UNIQUE NOT NULL,
+    password bytea NOT NULL,
+    email citext
+);
