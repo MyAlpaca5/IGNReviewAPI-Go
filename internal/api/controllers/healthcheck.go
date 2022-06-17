38c84a714dd16a1c67b97f2b1e9514eaa7b2e238
Pinhao.G <11252872+MyAlpaca5@users.noreply.github.com>
2022-06-17 12:15:47 -0500

    refactor: reorg healthcheck endpoint

--- internal/api/controllers/healthcheck.go
+++ internal/api/controllers/healthcheck.go
@@ -3,18 +3,27 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/MyAlpaca5/IGNReviewAPI-Go/internal/db/models"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 type HealthcheckController struct{}
 
+type SystemInfo struct {
+	Env     string `json:"env"`
+	Version string `json:"version"`
+}
+
+type Healthcheck struct {
+	Status     string     `json:"status"`
+	SystemInfo SystemInfo `json:"system_info"`
+}
+
 // HealthcheckHandler handles "GET /api/healthcheck" endpoint.
-func (controller HealthcheckController) HealthcheckHandler(c *gin.Context) {
-	hc := models.Healthcheck{
+func (ctrl HealthcheckController) HealthcheckHandler(c *gin.Context) {
+	hc := Healthcheck{
 		Status: "available",
-		SystemInfo: models.SystemInfo{
+		SystemInfo: SystemInfo{
 			Env:     viper.GetString("env"),
 			Version: viper.GetString("general.version"),
 		},
